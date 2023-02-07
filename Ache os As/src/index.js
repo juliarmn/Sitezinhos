8a648cd0c03e89938fc47e317c0c942053742073
juliarmn <julia.rm.nascimento@unesp.br>
2023-02-06 22:55:44 -0300

    muda site abeia->header e novo banner

--- Ache os As/src/index.js
+++ Ache os As/src/index.js
@@ -1,10 +1,22 @@
 // Cria as cartas dinamicamente
 const cardObjetctDefinitions = [
-  {id:1, imagePath:'/src/images/card-KingHearts.png'},
-  {id:2, imagePath:'/src/images/card-JackClubs.png'},
-  {id:3, imagePath:'/src/images/card-QueenDiamonds.png'},
-  {id:4, imagePath:'/src/images/card-AceSpades.png'}
+  { id:1, imagePath: '/Ache-os-as/src/images/card-Kinghearts.png' },
+  { id:2, imagePath: '/src/images/card-JackClubs.png' },
+  { id:3, imagePath: '/src/images/card-QueenDiamonds.png' },
+  { id:4, imagePath: '/src/images/card-AceSpades.png' }
 ]
+
+const cardBackImgPath = '/src/images/card-back-blue.png'
+
+const cardContainerElem = document.querySelector('.card-container')
+
+createCards()
+
+function createCards(){
+  cardObjetctDefinitions.forEach((cardItem)=>{
+    createCard(cardItem)
+  })
+}
 
 function creatCard(cardItem) {
   //Cria os elementos da div que faz a carta
@@ -18,12 +30,28 @@ function creatCard(cardItem) {
 
   addClassToElement(cardElem, 'card')
   addIdToElement(cardElem, cardItem.id)
+  addClassToElement(cardInner, 'card-inner')
+  addClassToElement(cardFront, 'card-front')
+  addClassToElement(cardBack, 'card-back')
 
-  //23:47
+  addSrcToElementImage(cardBackImg, cardBackImgPath)
+  addSrcToElementImage(cardFrontImg, cardItem.imagePath)
+  addClassToElement(cardBackImg, 'card-img')
+  addClassToElement(cardFrontImg, 'card-img')
+
+  addChildElement(cardFront, cardFrontImg)
+  addChildElement(cardBack, cardBackImg)
+  addChildElement(cardInner, cardFrontImg)
+  addChildElement(cardInner, cardBackImg)
+
+  addChildElement(cardElem, cardInner)
+
+  addCardToGridCell(cardElem)
+
 }
 
 function createElement(elemType) {
-  return cardElem = document.createElement(elemType)
+  return document.createElement(elemType)
 }
 
 function addClassToElement(elem, className) {
@@ -31,5 +59,34 @@ function addClassToElement(elem, className) {
 }
 
 function addIdToElement(elem, id) {
+  elem.id = id
+}
 
+function addSrcToElementImage(imgElem, src) {
+  imgElem.src = src
+}
+
+function addChildElement(parentElem, childElem) {
+  parentElem.appendChild(childElem)
+}
+
+function addCardToGridCell(card) {
+  const cardPositionClassName = mapCardIdToGridCell(card)
+  const cardPosElem = document.querySelector(cardPositionClassName)
+  addChildElement(cardPosElem, card)
+}
+
+function mapCardIdToGridCell(card){
+  if(card.id == 1) {
+    return '.card-pos-a'
+  }
+  if(card.id == 2) {
+    return '.card-pos-b'
+  }
+  if(card.id == 3) {
+    return '.card-pos-c'
+  }
+  if(card.id == 4) {
+    return '.card-pos-d'
+  }
 }
